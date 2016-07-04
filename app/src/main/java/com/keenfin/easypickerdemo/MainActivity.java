f6f7f618b8a63d83fbac2d066db793c46e934ca5
Stanislav Petryakov <become.glory@yandex.ru>
2016-07-04 16:34:44 +0300

    Fix demo for API 23

--- app/src/main/java/com/keenfin/easypickerdemo/MainActivity.java
+++ app/src/main/java/com/keenfin/easypickerdemo/MainActivity.java
@@ -7,12 +7,17 @@
 
 package com.keenfin.easypickerdemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -34,8 +39,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mPhotoPickerGrid = (PhotoPicker) findViewById(R.id.pp_easypicker_grid);
         mPhotoPickerGridNoControls = (PhotoPicker) findViewById(R.id.pp_easypicker_grid_no_controls);
 
-        if (savedInstanceState == null)
-            getSupportLoaderManager().initLoader(0, null, this);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        } else {
+            if (savedInstanceState == null)
+                getSupportLoaderManager().initLoader(0, null, this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 0:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                    getSupportLoaderManager().initLoader(0, null, this);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
